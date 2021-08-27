ceed5810e7639f50126cd364b83bece574275ac7
Thalison Campos <thalison1998@gmail.com>
2021-08-27 13:49:42 -0300

    fetch animais refactor

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,31 +1,43 @@
 import animaNumeros from "./anima-numeros.js";
 
-export default function initFetchAnimais() {
+export default function fetchAnimais(url, target) {
+  const numerosGrid = document.querySelector(target);
+
+  // cria a div contendo informações com o total de animais
   function createAnimal(animal) {
     const div = document.createElement("div");
     div.classList.add("numero-animal");
     div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
     return div;
   }
-  async function fetchAnimais(url) {
+
+  // preenche cada animal no doom //
+  const preencherAnimais = (animal) => {
+    const divAnimal = createAnimal(animal);
+    numerosGrid.appendChild(divAnimal);
+  };
+
+  // anima o numero de cada animal
+
+  const animaAnimaisNumeros = () => {
+    const animatesNumbers = animaNumeros("[data-numero]", ".numeros", "ativo");
+    animatesNumbers.init();
+  };
+  // puxa os animais atráves de um arquivo json
+  // cria cada animal utilizando o createAnimal
+  async function criarAnimais() {
     try {
+      // fetch aguarda a resposta
       const animaisResponse = await fetch(url);
+      // transforma a resposta em json
       const animaisJSON = await animaisResponse.json();
-      const numerosGrid = document.querySelector(".numeros-grid");
-      animaisJSON.forEach((animal) => {
-        const divAnimal = createAnimal(animal);
-        numerosGrid.appendChild(divAnimal);
-      });
-      const animatesNumbers = animaNumeros(
-        "[data-numero]",
-        ".numeros",
-        "ativo"
-      );
-      animatesNumbers.init();
+      //após a transformação em json, ativa as funções
+      // para preencher e animar os números
+      animaisJSON.forEach((animal) => preencherAnimais(animal));
+      animaAnimaisNumeros();
     } catch (erro) {
       console.log(erro);
     }
   }
-
-  fetchAnimais("./animaisapi.json");
+  return criarAnimais();
 }
--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
-import Accordion from './modules/accordion';
-import initAnimacaoScroll from './modules/scroll-animacao';
-import tabNav from './modules/tabnav';
-import Modal from './modules/modal';
-import Tooltip from './modules/tooltip';
-import initDropdownMenu from './modules/dropdown-menu';
-import initMenuMobile from './modules/menu-mobile';
-import initFuncionamento from './modules/funcionamento';
-import initFetchAnimais from './modules/fetch-animais';
-import initFetchBitcoin from './modules/fetch-bitcoin';
+import Accordion from "./modules/accordion";
+import initAnimacaoScroll from "./modules/scroll-animacao";
+import tabNav from "./modules/tabnav";
+import Modal from "./modules/modal";
+import Tooltip from "./modules/tooltip";
+import initDropdownMenu from "./modules/dropdown-menu";
+import initMenuMobile from "./modules/menu-mobile";
+import initFuncionamento from "./modules/funcionamento";
+import fetchAnimais from "./modules/fetch-animais";
+import initFetchBitcoin from "./modules/fetch-bitcoin";
 
 const accordion = Accordion('[data-anime="accordion"] dt');
 accordion.init().changeInitAcordion(0);
 
 const tabN = tabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabN.init();
-const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+const modal = new Modal(
+  '[data-modal="abrir"]',
+  '[data-modal="fechar"]',
+  '[data-modal="container"]',
+);
 modal.init();
 
 const tooltip = new Tooltip("[data-tooltip]");
@@ -23,6 +27,7 @@ tooltip.init();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
 initFetchBitcoin();
 initAnimacaoScroll();
+
+fetchAnimais("../../animaisapi.json",'.numeros-grid');
